bf5e3123fa5d8283912856dc314a3684d4fc8c03
Zack <168221270+Zval321@users.noreply.github.com>
2026-02-01 20:59:14 -0600

    returning data on backend using pybaseball - a scraper for baseball statistics

--- stat-diamond-backend/main.py
+++ stat-diamond-backend/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI 
+from pybaseball import statcast
+import pandas as pd
 
 app = FastAPI()
 
 @app.get('/')
-def read_root(): 
-  return { "message": "Hello Zack"}
+def root(): 
+  return { "status": "Backend Running"}
+
+@app.get('/statcast')
+def get_statcast_data(): 
+  data = statcast(start_dt="2019-06-24",end_dt="2019-06-25")
+  data = data.replace({pd.NA: None})
+  data = data.where(pd.notnull(data), None)
+  data = data.replace([float('inf'), float('-inf')], None)
+
+  return data.to_dict(orient='records')
