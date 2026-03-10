3937adecca9cb684e9f364a3f747834721031c69
Zack <168221270+Zval321@users.noreply.github.com>
2026-03-09 20:36:46 -0500

    fixed up the frontend main page =

--- stat-diamond-frontend/src/Screens/MainPage/MainPage.tsx
+++ stat-diamond-frontend/src/Screens/MainPage/MainPage.tsx
@@ -2,27 +2,66 @@ import './MainPage.css'
 import logo from '../../assets/StatDiamondLogo.png'
 // import { ModalHome } from './Modal-home'
 // import { useState } from 'react'
+import { useEffect } from 'react'
 
 
 export function MainPage() {
   const user = localStorage.getItem('token')
   console.log(user)
-  // const [modalShowing, setModalShowing] = useState(false)
 
-  // const openModal = () => {
-  //   setModalShowing(true)
-  // }
+
+  useEffect(() => {
+    const handleFetchTopAverages = () => {
+
+    }
+
+    const handleFetchTopERA = () => {
+
+    }
+
+    const handleFetchTopStrikeouts = () => {
+
+    }
+
+    const handleFetchTopHomeRuns = () => {
+
+    }
+  })
+
 
   return (
-    <>
+    <div className='main-page-container'>
       <div className='logo-container'>
         <img className='logo' src={logo} />
-        {/* <ModalHome
-          show={modalShowing}
-          onClose={() => setModalShowing(false)}
-        /> */}
-        {/* <button onClick={openModal}>Open Modal</button> */}
       </div>
-    </>
+      <div className='leader-boxes-container'>
+        <div className='leader-box-left'>
+          <div className='batting_average'>
+            <h1>Batting Average</h1>
+          </div>
+        </div>
+        <div className='leader-box-left'>
+          <div className='Home runs'>
+            <h1>Home Runs</h1>
+
+          </div>
+        </div>
+
+        <div className='leader-box-right'>
+          <div className='ERA'>
+            <h1>ERA</h1>
+
+          </div>
+        </div>
+
+        <div className='leader-box-right'>
+          <div className='strikeouts'>
+            <h1>Strikeouts</h1>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
   )
 }
