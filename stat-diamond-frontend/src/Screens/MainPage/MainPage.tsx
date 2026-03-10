97c4fd55209dcf7ef2ea1bee1f7f88f41591e021
Zack <168221270+Zval321@users.noreply.github.com>
2026-03-09 21:42:44 -0500

    updates to the first page of the application for leader-boards

--- stat-diamond-frontend/src/Screens/MainPage/MainPage.tsx
+++ stat-diamond-frontend/src/Screens/MainPage/MainPage.tsx
@@ -1,32 +1,50 @@
 import './MainPage.css'
 import logo from '../../assets/StatDiamondLogo.png'
 // import { ModalHome } from './Modal-home'
-// import { useState } from 'react'
+import { useState } from 'react'
 import { useEffect } from 'react'
+import type { Player } from '../../types/types'
 
 
 export function MainPage() {
-  const user = localStorage.getItem('token')
-  console.log(user)
+  const [topBattingAvg, setTopBattingAvg] = useState<Player[]>([])
+  const number = Array.from({ length: 10 }, (_, i) => i + 1)
+  // const [topHomeRun, setTopHomeRun] = useState([])
+  // const [topSo, setSo] = useState([])
+  // const [topERA, setTopERA] = useState([])
+
 
 
   useEffect(() => {
-    const handleFetchTopAverages = () => {
-
+    const handleFetchTopAverages = async () => {
+      try {
+        const season = 2025;
+        const res = await fetch(`http://localhost:8000/api/stats/player/batting?start=${season}&end=${season}&min_pa=1`)
+        const data = await res.json()
+        console.log(data)
+        const top_batting_averages = (data.sort((a, b) => b.batting_average - a.batting_average).slice(0, 10))
+        setTopBattingAvg(top_batting_averages)
+      } catch (error) {
+        console.error("There was an error", error)
+      }
     }
 
-    const handleFetchTopERA = () => {
+    // const handleFetchTopERA = () => {
 
-    }
+    // }
 
-    const handleFetchTopStrikeouts = () => {
+    // const handleFetchTopStrikeouts = () => {
 
-    }
+    // }
 
-    const handleFetchTopHomeRuns = () => {
+    // const handleFetchTopHomeRuns = () => {
 
-    }
-  })
+    // }
+
+    handleFetchTopAverages()
+  }, [])
+
+
 
 
   return (
@@ -38,6 +56,24 @@ export function MainPage() {
         <div className='leader-box-left'>
           <div className='batting_average'>
             <h1>Batting Average</h1>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Name</th>
+                  <th>AVG</th>
+                </tr>
+              </thead>
+              <tbody>
+                {topBattingAvg.map((leaders, index) => (
+                  <tr key={leaders.IDfg}>
+                    <td>{index + 1}</td>
+                    <td>{leaders.Name}</td>
+                    <td>{leaders.AVG}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
         </div>
         <div className='leader-box-left'>
